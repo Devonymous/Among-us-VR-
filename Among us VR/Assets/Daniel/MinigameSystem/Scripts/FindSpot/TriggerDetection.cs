f7c87a0b70099d59449c58e6b3c588678b4373f1
DHindriks <dl.hindriks@gmail.com>
2023-01-12 11:29:06 +0100

    Minigame base

---
+++ Among us VR/Assets/Daniel/MinigameSystem/Scripts/FindSpot/TriggerDetection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetection : MonoBehaviour
+{
+    [SerializeField] MinigameBase minigame;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            minigame.OnComplete.Invoke();
+        }
+    }
+}
---
+++ Among us VR/Assets/Daniel/MinigameSystem/Scripts/MinigameBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MinigameBase : MonoBehaviour
+{
+    public UnityEvent OnComplete;
+    public UnityEvent OnCompleteRound;
+    public UnityEvent OnFail;
+    public UnityEvent OnFailRound;
+
+}
