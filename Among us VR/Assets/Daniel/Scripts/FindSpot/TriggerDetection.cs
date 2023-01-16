bef8cde3f74c83c3b26b8e5819723849e836b534
DHindriks <dl.hindriks@gmail.com>
2023-01-16 21:35:20 +0100

    Update TriggerDetection.cs

--- Among us VR/Assets/Daniel/Scripts/FindSpot/TriggerDetection.cs
+++ Among us VR/Assets/Daniel/Scripts/FindSpot/TriggerDetection.cs
@@ -34,20 +34,20 @@ public class TriggerDetection : MonoBehaviour
         if (Vector3.Distance(transform.position, ControllerL.transform.position) < 2f)
         {
             //send strong pulses to indacate the player is very near
-            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.5f, 10, 0.6f);
             Debug.Log("Very near");
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.5f, 10, 0.6f);
         }
         else if (Vector3.Distance(transform.position, ControllerL.transform.position) < 6f)
         {
             //send medium pulses that indicates the player is getting closer
-            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.5f, 10, 0.3f);
             Debug.Log("Near");
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.5f, 10, 0.3f);
         }
         else if (Vector3.Distance(transform.position, ControllerL.transform.position) > 6f)
         {
             //send weak pulses that indicates the player is far away
-            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.5f, 10, 0.1f);
             Debug.Log("far away");
+            SteamVR_Actions.default_Haptic[SteamVR_Input_Sources.LeftHand].Execute(0, 0.5f, 10, 0.1f);
         }
     }
 
