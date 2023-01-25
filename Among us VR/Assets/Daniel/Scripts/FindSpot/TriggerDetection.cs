be360f3f3e4a42c273124d75ff9f61f0fed1b782
DHindriks <dl.hindriks@gmail.com>
2023-01-25 20:50:17 +0100

    Update TriggerDetection.cs

--- Among us VR/Assets/Daniel/Scripts/FindSpot/TriggerDetection.cs
+++ Among us VR/Assets/Daniel/Scripts/FindSpot/TriggerDetection.cs
@@ -20,6 +20,7 @@ public class TriggerDetection : MonoBehaviour
 
             //send strongest pulse to indicate that the player reached the spot
             HapticAction.Execute(0, 1, 150, 1, SteamVR_Input_Sources.LeftHand);
+            HapticAction.Execute(0, 1, 150, 1, SteamVR_Input_Sources.RightHand);
         }
     }
 
@@ -36,13 +37,13 @@ public class TriggerDetection : MonoBehaviour
     void CheckPlayerDistance()
     {
         //LEFT
-        if (Vector3.Distance(transform.position, ControllerL.transform.position) < 0.2f)
+        if (Vector3.Distance(transform.position, ControllerL.transform.position) < 0.4f)
         {
             //send strong pulses to indacate the player is very near
             Debug.Log("Very near");
             HapticAction.Execute(0, 0.2f, 150, 0.6f, SteamVR_Input_Sources.LeftHand);
         }
-        else if (Vector3.Distance(transform.position, ControllerL.transform.position) < 0.5f)
+        else if (Vector3.Distance(transform.position, ControllerL.transform.position) < 1)
         {
             //send medium pulses that indicates the player is getting closer
             Debug.Log("Near");
@@ -56,13 +57,13 @@ public class TriggerDetection : MonoBehaviour
         }
 
         //RIGHT
-        if (Vector3.Distance(transform.position, ControllerR.transform.position) < 0.2f)
+        if (Vector3.Distance(transform.position, ControllerR.transform.position) < 0.4f)
         {
             //send strong pulses to indacate the player is very near
             Debug.Log("Very near");
             HapticAction.Execute(0, 0.2f, 150, 0.6f, SteamVR_Input_Sources.RightHand);
         }
-        else if (Vector3.Distance(transform.position, ControllerR.transform.position) < 0.5f)
+        else if (Vector3.Distance(transform.position, ControllerR.transform.position) < 1)
         {
             //send medium pulses that indicates the player is getting closer
             Debug.Log("Near");
